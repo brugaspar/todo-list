6e47853411491b9c4bfb4abcd337797f3c1d73ab
Bruno Gaspar <bruninhoogaspar@gmail.com>
2020-10-29 15:05:54 -0300

    Added some adjusts

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import { RiCloseCircleLine } from 'react-icons/ri'
-import { TiEdit } from 'react-icons/ti'
+import React, { useState } from "react"
+import { RiCloseCircleLine } from "react-icons/ri"
+import { TiEdit } from "react-icons/ti"
 
-import TodoForm from './TodoForm'
+import TodoForm from "./TodoForm"
 
 function Todo({ todos, completeTodo, updateTodo, removeTodo }) {
   const [edit, setEdit] = useState({
     id: null,
-    value: ''
+    value: ""
   })
 
   const submitUpdate = value => {
     updateTodo(edit.id, value)
 
-    setEdit({ id: null, value: '' })
+    setEdit({ id: null, value: "" })
   }
 
   if(edit.id) {
-    return <TodoForm edit={edit} onSubmit={submitUpdate}/>
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />
   }
 
   return todos.map((todo, index) => (
     <div
-      key={index}  
+      key={index}
       className={
-        todo.isComplete 
-          ? 'todo-row complete' 
-          : 'todo-row'
+        todo.isComplete
+          ? "todo-row complete"
+          : "todo-row"
       }
     >
       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
@@ -34,8 +34,8 @@ function Todo({ todos, completeTodo, updateTodo, removeTodo }) {
       </div>
 
       <div className="icons">
-        <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon"/>
-        <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })} className="edit-icon"/>
+        <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon" />
+        <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })} className="edit-icon" />
       </div>
     </div>
   ))
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
-import TodoForm from './TodoForm'
+import TodoForm from "./TodoForm"
 import Todo from "./Todo"
 
 function TodoList() {
